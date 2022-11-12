b8d2c3df862f2c1e394f273a9885059f5cb6b5f4
Paulo Salvatore <salvatoregames@gmail.com>
2022-11-11 21:01:24 -0300

    feat(main): add `ValidationPipe`

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,18 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // ValidationPipe
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
 
   const config = new DocumentBuilder()
     .setTitle('FIAP NestJS Demo')
